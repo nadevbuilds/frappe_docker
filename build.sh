1880c07aa64e11fee68b698dbd9ba119ef5e4453
nadevbuilds <support@nainv.de>
2025-08-22 14:26:13 +0200

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+docker build \
+  --build-arg FRAPPE_PATH=$FRAPPE_REPO_PATH \
+  --build-arg FRAPPE_BRANCH=version-15-fsm \
+  --build-arg PYTHON_VERSION=3.11.9 \
+  --build-arg NODE_VERSION=18.20.2 \
+  --build-arg APPS_JSON_BASE64=$APPS_JSON_BASE64 \
+  --tag $TAG_NAME \
+  --file images/custom/Containerfile .
