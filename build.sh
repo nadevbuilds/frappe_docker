ebcc9e882f872978d623668a29cb4cabce84ae5c
nadevbuilds <support@nainv.de>
2025-08-22 14:49:16 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker build --no-cache --build-arg FRAPPE_PATH=$FRAPPE_REPO_PATH --build-arg FRAPPE_BRANCH=version-15-fsm --build-arg PYTHON_VERSION=3.11.9 --build-arg NODE_VERSION=18.20.2 --build-arg APPS_JSON_BASE64=$APPS_JSON_BASE64 --tag $TAG_NAME --file images/custom/Containerfile .
+docker buildx build --no-cache --build-arg FRAPPE_PATH=$FRAPPE_REPO_PATH --build-arg FRAPPE_BRANCH=version-15-fsm --build-arg PYTHON_VERSION=3.11.9 --build-arg NODE_VERSION=18.20.2 --build-arg APPS_JSON_BASE64=$APPS_JSON_BASE64 --tag $TAG_NAME --file images/custom/Containerfile .
